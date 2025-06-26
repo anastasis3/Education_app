e61d32755d789ecdc6c16a49fc505cac402df1f7
anastasis3 <kuprichyk@mail.ru>
2025-06-26 17:09:26 +0300

    Initial commi1t

--- index.js
+++ index.js
@@ -81,6 +81,11 @@ app.post('/login', async (req, res) => {
   }
 });
 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'auth.html'));
+});
+
 // Запуск сервера
 app.listen(port, () => {
   console.log(`🚀 Сервер запущен на порту ${port}`);
