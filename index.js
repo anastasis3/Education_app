7ecfd84a38057188d13fa49215934f681eab2548
anastasis3 <kuprichyk@mail.ru>
2025-07-03 11:37:22 +0300

    Add connect-pg-simpl6ljeо

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(session({
 
 // Шаблонизатор ejs
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public', 'views')); 
 
 // Статические файлы
 app.use(express.static(path.join(__dirname, 'public')));
